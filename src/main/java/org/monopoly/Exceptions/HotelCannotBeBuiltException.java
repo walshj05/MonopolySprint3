7a9ac537bbbb01b26ffa3e7e6a9aa3d7c70ce990
Jon Walsh <walshj05@moravian.edu>
2025-04-01 20:15:16 -0400

    removed unused exception

--- src/main/java/org/monopoly/Exceptions/HotelCannotBeBuiltException.java
+++ src/main/java/org/monopoly/Exceptions/HotelCannotBeBuiltException.java
@@ -1,5 +1,5 @@
 package org.monopoly.Exceptions;
-
+// todo implement exception headers for all exception files
 public class HotelCannotBeBuiltException extends Exception {
     public HotelCannotBeBuiltException(String message) {
         super(message);
--- src/main/java/org/monopoly/Exceptions/NoSuchMortgageException.java
+++
@@ -1,7 +0,0 @@
-package org.monopoly.Exceptions;
-
-public class NoSuchMortgageException extends Exception {
-    public NoSuchMortgageException(String message) {
-        super(message);
-    }
-}
