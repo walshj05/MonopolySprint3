a3fb418c4ad3603028c798f270215c326bd14898
Sydney Shifman <shifmans@moravian.edu>
2025-04-05 22:29:17 -0400

    Added Borders and Replaced TextArea

--- src/main/java/org/monopoly/monopolygameproject/HelloApplication.java
+++ src/main/java/org/monopoly/monopolygameproject/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Board.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1230, 780);
-        stage.setTitle("Hello!");
+        stage.setTitle("Monopoly Game");
         stage.setScene(scene);
         stage.show();
     }
