86039e88aef69be99bf7c7f666cec0f345061c23
Sydney Shifman <shifmans@moravian.edu>
2025-04-10 07:31:41 -0400

    Added Commented Code

--- src/main/java/org/monopoly/Model/Cards/CommunityChestDeck.java
+++ src/main/java/org/monopoly/Model/Cards/CommunityChestDeck.java
@@ -139,8 +139,8 @@ public class CommunityChestDeck extends CardDeck {
                 player.addToBalance(25);
                 break;
             case "You are assessed for street repair. $40 per house. $115 per hotel":
-                int totalCharge = (115 * player.getNumHotels()) + (40 * player.getNumHouses());
-                player.subtractFromBalance(totalCharge);
+//                int totalCharge = (115 * player.getNumHotels()) + (40 * player.getNumHouses());
+//                player.subtractFromBalance(totalCharge);
                 break;
             case "You have won second prize in a beauty contest. Collect $10":
                 player.addToBalance(10);
